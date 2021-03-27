2bc5082914e65a8730af5675187a53d3ce30eb24
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2021-03-27 19:54:58 -0400

    heroku preparations

--- config/settings.py
+++ config/settings.py
@@ -149,3 +149,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+if HEROKU:
+    import django_heroku
+    django_heroku.settings(locals())
