c50cdefb80de1c4888108d756ccbd05e5ac11d7b
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2021-04-09 18:25:17 -0400

    Updated eslint to ignore errors in the production builds

--- public/.eslintrc.js
+++ public/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  ignorePatterns: ['./build/static/js/**'],
   extends: ['plugin:react/recommended', 'airbnb', 'react-app'],
   parserOptions: {
     ecmaFeatures: {
